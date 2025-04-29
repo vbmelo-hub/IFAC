ebb9b69c3d1d8597290c4890f7036ace2a89fa22
Vinícius Barros <vinicius.meloin@gmail.com>
2025-04-28 23:58:30 -0500

    beecrowd28/04

---
+++ Períodos/3º Período/Programação Orientada Objeto - Jonas/JavaBeecorowd/numerospositivos/Main.java
@@ -0,0 +1,17 @@
+package numerospositivos;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+        double[] x = new double[6];
+
+        while (x.length > 0) {
+            for () {
+                System.out.println("");
+            }
+        }
+        ler.close();
+    }
+}
